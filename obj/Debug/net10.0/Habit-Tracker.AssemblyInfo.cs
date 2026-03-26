0b1bd1dc3a054aac87fe74f775bed0ebdcc4a045
ChavierJC <juaarez_javier@hotmail.com>
2026-03-25 23:57:03 -0600

    Something

--- obj/Debug/net10.0/Habit-Tracker.AssemblyInfo.cs
+++ obj/Debug/net10.0/Habit-Tracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Habit-Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eee085346fe7f6e145b43c1331e5ca5e8c1109fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Habit-Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Habit-Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
